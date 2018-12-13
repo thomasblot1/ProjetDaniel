5c3d8046962f881082dd224dd6a486fa73236a09
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2018-12-13 08:31:51 +0100

    clean de lcd apres le menu

--- init.c
+++ init.c
@@ -1,27 +1,27 @@
 #include "init.h"
 
 // --- init the PIC18F device
-void initMyPIC18F(void)
-{
-	// PORTA digital
-	ADCON1 = 0x0F ;
-	ADCON0 = 0;
 
-	// set all ports as OUTPUTS only the screen will be considered as output
-	TRISA = 0x00;
-	TRISB = 0x00;
-	TRISC = 0x00;
-	TRISD = 0x00;
-	TRISE = 0x00;
-// thomas travaille sur le tris pour el port e et le port b des boutons
-	// set port by port on "all zeros"
-	PORTA = 0x00;
-	PORTB = 0x00;
-	PORTC = 0x00;
-	PORTD = 0x00;
-// make sure to have an empty LAST line in any *.c file (just hit an Enter)!
+void initMyPIC18F(void) {
+    // PORTA digital
+    ADCON1 = 0x0F;
+    ADCON0 = 0;
 
-	PORTE = 0x00;
+    // set all ports as OUTPUTS only the screen will be considered as output
+    TRISA = 0x00;
+    TRISB = 0x00;
+    TRISC = 0xFF;
+    TRISD = 0x00;
+    TRISE = 0x00;
+    // thomas travaille sur le tris pour el port e et le port b des boutons
+    // set port by port on "all zeros"
+    PORTA = 0x00;
+    PORTB = 0x00;
+    PORTC = 0x00;
+    PORTD = 0x00;
+    // make sure to have an empty LAST line in any *.c file (just hit an Enter)!
+
+    PORTE = 0x00;
 
 }
 // make sure to have an empty LAST line in any *.c file (just hit an Enter)!
--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 
 #include "main.h"
 #include "glcd.h"
+
+#include <xc.h>
+
 #pragma config FOSC = HS 		//oscillator HS
 #pragma config PWRT = OFF
 #pragma config BOR = OFF
@@ -9,30 +12,45 @@
 #pragma config LVP = OFF 		//Disable low voltage programmig
 #pragma config DEBUG = OFF		//Debug OFFoo
 
-
-void main (void)
-{
-    PORTE=0x00;
+void main(void) {
+    PORTE = 0x00;
+    int y = 0, i = 0;
     // call the function that will init the PIC
     initMyPIC18F();
     glcd_Init(GLCD_ON);
 
-        glcd_PlotPixel(2,2, 0xFF);
-        glcd_SetCursor(51,10);
-        glcd_WriteString("Space Invader",f8X8,1);
-        glcd_SetCursor(53,30);
-        glcd_WriteString("Let's PLAY",f8X8,1);
-        glcd_SetCursor(48,50);
-        glcd_WriteString("Made by Daniel, Gabriel et Thomas",f8X8,1);
+    //glcd_PlotPixel(2,2, 0xFF);
+    glcd_SetCursor(10, 1);
+    glcd_WriteString("Space Invader", f8X8, 1);
+    __delay_ms(500);
+    glcd_SetCursor(30, 3);
 
+    
 
+    //glcd_SetCursor(53,60);
+    glcd_WriteString("Made by", f8X8, 1);
+    glcd_SetCursor(10, 5);
+    __delay_ms(20);
+    glcd_WriteString("Daniel,Gabriel", f8X8, 1);
+    glcd_SetCursor(30, 7);
+    glcd_WriteString("et Thomas", f8X8, 1);
+
+    __delay_ms(600);
+
+    glcd_Init(GLCD_OFF);
+    //on utilise la fonction quand on arrete pour clean le glcd
+    glcd_Init(GLCD_ON);
+    glcd_SetCursor(20, 4);
+    glcd_WriteString("Let's PLAY", f8X8, 1);
+    __delay_ms(400);
+    glcd_Init(GLCD_OFF);
+    //on utilise la fonction quand on arrete pour clean le glcd
+    glcd_Init(GLCD_ON);
     glcd_Image();
 
 
 
-    while(1)
-    {
-
+    while (1) {
     }
 
 }// end of the main function
